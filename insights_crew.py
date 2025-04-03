ab82a4d5cc0d845d6c9df998a5299a42c0a66d26
Kavin Lartius <kavlartius@gmail.com>
2025-04-03 17:39:26 +0530

    Update insights_crew.py

--- insights_crew.py
+++ insights_crew.py
@@ -1,32 +1,34 @@
-from crewai import Agent, Task, Crew, Process
+from crewai import Agent, Task, Crew
 from crewai.project import agent, task, crew, CrewBase
 from crewai_tools import SerperDevTool
 
-search_tool=SerperDevTool()
+search_tool_1= SerperDevTool()
 
 @CrewBase
-class sentiment_crew():
-
+class insights_crew():
+  
   @agent
-  def sentiment_analysis_agent(self)->Agent:
+  def insight_agent(self)->Agent:
     return Agent(
-       role="Stock Public Sentiment Analysis",
-       goal="Analyze the public sentiment of a stock and assign a score",
-       backstory="You are an expert in researching and analyzing public sentiments in the stock name",
-       tools=[search_tool]
+      role="Insight based information provider",
+      goal="Provide useful inferences and insights which would help an investor based on the past data of a stock",
+      backstory="You are share holder with years of experience and an expertise on evaluating past stock metrics drawing inferences for future actions",
+      tools=[search_tool_1]
     )
 
+  
   @task
-  def sentiment_analysis_task(self)->Task:
+  def insights_task(self)->Task:
     return Task(
-      description=" based on the {stock_name} provided by the user analyze the current news about the stock and assign a score 1- negative, 2- neutral, 3-negative",
-      expected_output="A sentiment analysis score with no further texts or outputs",
-      agent=self.sentiment_analysis_agent()
+      description="Given the {stock_name} and its {negative_sentiment_score} determined by the prior agentic crew, analyze the stock tables for the past 6 months. Provide strategic insights on how an investor can turn a profit despite the current negative sentiment.",
+      expected_output="Actionable insights derived from the past 6 months of {stock_name} stock data, considering its bad sentiment analysis score. Offer strategies to help the investor capitalize on this scenario and generate profit.", 
+      agent=self.insight_agent()
     )
 
-
+  
+  @crew
   def crew(self)->Crew:
     return Crew(
-      agents=[self.sentiment_analysis_agent()],
-      tasks=[self.sentiment_analysis_task()]
+      agents=[self.insight_agnet()],
+      tasks=[self.insights_task()]
     )
